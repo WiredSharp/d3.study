dd25fe902b0115e430947a49792cd8b5be05a8a9
ebl <zericco@gmail.com>
2017-07-20 23:13:46 +0200

    basic sigma project

--- gulpfile.js
+++ gulpfile.js
@@ -10,78 +10,79 @@ var gulp = require('gulp'),
     browserSync = require('browser-sync');
 
 const pathes = {
-        libs: ["node_modules/d3/build/d3.*"],
-        css: ["src/css/**/*.css"],
-        js: ["src/js/**/*.js"],
-        html: ["src/**/*.html", "src/**/*.htm"],
-        images: ["src/images/**/*"],
-        data: ["data/**/*"],
-        target: "./dist"
-      };
+    libs: ["node_modules/sigma/build/**/sigma*.js"],
+    css: ["src/css/**/*.css"],
+    js: ["src/js/**/*.js"],
+    html: ["src/**/*.html", "src/**/*.htm"],
+    images: ["src/images/**/*"],
+    data: ["data/**/*"],
+    target: "./dist"
+};
 
 gulp.task('browser-sync', function() {
-  browserSync({
-    server: {
-       baseDir: pathes.target
-    }
-  });
+    browserSync({
+        server: {
+            baseDir: pathes.target
+        }
+    });
 });
 
-gulp.task('bs-reload', function () {
-  browserSync.reload();
+gulp.task('bs-reload', function() {
+    browserSync.reload();
 });
 
-gulp.task('images', function(){
-  gulp.src(pathes.images)
-    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-    .pipe(gulp.dest(pathes.target + '/images/'));
+gulp.task('images', function() {
+    gulp.src(pathes.images)
+        .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+        .pipe(gulp.dest(pathes.target + '/images/'));
 });
 
-gulp.task('html', function(){
-  gulp.src(pathes.html)
-  .pipe(gulp.dest(pathes.target + '/'))
+gulp.task('html', function() {
+    gulp.src(pathes.html)
+        .pipe(gulp.dest(pathes.target + '/'))
 });
 
-gulp.task('css', function(){
-  gulp.src(pathes.css)
-    .pipe(autoprefixer('last 2 versions'))
-    .pipe(gulp.dest(pathes.target + '/css/'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(minifycss())
-    .pipe(gulp.dest(pathes.target + '/css/'))
-    .pipe(browserSync.reload({stream:true}))
+gulp.task('css', function() {
+    gulp.src(pathes.css)
+        .pipe(autoprefixer('last 2 versions'))
+        .pipe(gulp.dest(pathes.target + '/css/'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(minifycss())
+        .pipe(gulp.dest(pathes.target + '/css/'))
+        .pipe(browserSync.reload({ stream: true }))
 });
 
 gulp.task('libs', function() {
-  gulp.src(pathes.libs)
-    .pipe(gulp.dest(pathes.target + "/lib/"))
+    gulp.src(pathes.libs)
+        .pipe(gulp.dest(pathes.target + "/lib/"))
 });
 
-gulp.task('js', function(){
-  return gulp.src(pathes.js)
-    .pipe(plumber({
-      errorHandler: function (error) {
-        console.log(error.message);
-        this.emit('end');
-    }}))
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest(pathes.target + '/js/'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(uglify())
-    .pipe(gulp.dest(pathes.target + '/js/'))
-    .pipe(browserSync.reload({stream:true}))
+gulp.task('js', function() {
+    return gulp.src(pathes.js)
+        .pipe(plumber({
+            errorHandler: function(error) {
+                console.log(error.message);
+                this.emit('end');
+            }
+        }))
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest(pathes.target + '/js/'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(gulp.dest(pathes.target + '/js/'))
+        .pipe(browserSync.reload({ stream: true }))
 });
 
-gulp.task('data', function(){
-  gulp.src(pathes.data)
-    .pipe(gulp.dest(pathes.target + '/data/'))
+gulp.task('data', function() {
+    gulp.src(pathes.data)
+        .pipe(gulp.dest(pathes.target + '/data/'))
 })
 
 gulp.task('build', ['data', 'html', 'js', 'css', 'libs']);
 
-gulp.task('default', ['build', 'browser-sync'], function(){
-  gulp.watch(pathes.html, ['html']);
-  gulp.watch(pathes.css, ['css']);
-  gulp.watch(pathes.js, ['js']);
-  gulp.watch(pathes.target + "/**/*", ['bs-reload']);
+gulp.task('default', ['build', 'browser-sync'], function() {
+    gulp.watch(pathes.html, ['html']);
+    gulp.watch(pathes.css, ['css']);
+    gulp.watch(pathes.js, ['js']);
+    gulp.watch(pathes.target + "/**/*", ['bs-reload']);
 });
